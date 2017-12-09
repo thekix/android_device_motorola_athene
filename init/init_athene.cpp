95a4ba8a795735616b1528830c88d333a85256de
Nikit Sharma <nikitsharma92@gmail.com>
2017-12-09 20:07:02 +0530

    athene: Update init for oreo property_set => android::init::property_set

--- init/init_athene.cpp
+++ init/init_athene.cpp
@@ -50,7 +50,6 @@ void property_override(char const prop[], char const value[])
 
 void vendor_load_properties()
 {
-
     std::string platform;
     std::string device_boot;
     std::string sku;
@@ -63,14 +62,14 @@ void vendor_load_properties()
         return;
 
     device_boot = android::base::GetProperty("ro.boot.device", "");
-    property_set("ro.hw.device", device_boot.c_str());
+    android::init::property_set("ro.hw.device", device_boot.c_str());
 
     sku = android::base::GetProperty("ro.boot.hardware.sku", "");
 
     carrier = android::base::GetProperty("ro.boot.carrier", "");
 
     radio = android::base::GetProperty("ro.boot.radio", "");
-    property_set("ro.hw.radio", radio.c_str());
+    android::init::property_set("ro.hw.radio", radio.c_str());
 
     /* Common for all models */
     property_override("ro.build.product", "athene");
@@ -79,40 +78,40 @@ void vendor_load_properties()
 
     if (device_boot == "athene_13mp") {
         /* Moto G4 (XT162x) */
-        property_set("ro.product.device", "athene");
-        property_set("ro.build.description", "athene-user 7.0 NPJS25.93-14-8 8 release-keys");
-        property_set("ro.build.fingerprint", "motorola/athene/athene:7.0/NPJS25.93-14-8/8:user/release-keys");
-        property_set("ro.product.model", "Moto G4");
-        property_set("ro.telephony.default_network", "10");
+        property_override("ro.product.device", "athene");
+        property_override("ro.build.description", "athene-user 7.0 NPJ25.93-14 16 release-keys");
+        property_override("ro.build.fingerprint", "motorola/athene/athene:7.0/NPJ25.93-14/16:user/release-keys");
+        property_override("ro.product.model", "Moto G4");
+        android::init::property_set("ro.telephony.default_network", "10");
     } else {
         /* Moto G4 Plus (XT164x) */
-        property_set("ro.product.device", "athene_f");
-        property_set("ro.build.description", "athene_f-user 7.0 NPJS25.93-14-8 8 release-keys");
-        property_set("ro.build.fingerprint", "motorola/athene_f/athene_f:7.0/NPJS25.93-14-8/8:user/release-keys");
-        property_set("ro.product.model", "Moto G4 Plus");
-        property_set("ro.telephony.default_network", "10,0");
+        property_override("ro.product.device", "athene_f");
+        property_override("ro.build.description", "athene_f-user 7.0 NPJ25.93-14 16 release-keys");
+        property_override("ro.build.fingerprint", "motorola/athene_f/athene_f:7.0/NPJ25.93-14/16:user/release-keys");
+        property_override("ro.product.model", "Moto G4 Plus");
+        android::init::property_set("ro.telephony.default_network", "10,0");
     }
 
    if (sku == "XT1625" || sku == "XT1644") {
-       property_set("net.tethering.noprovisioning", "true");
-        property_set("persist.radio.is_wps_enabled", "true");
-        property_set("ro.radio.imei.sv", "4");
-        property_set("tether_dun_required", "0");
+        android::init::property_set("net.tethering.noprovisioning", "true");
+        android::init::property_set("persist.radio.is_wps_enabled", "true");
+        android::init::property_set("ro.radio.imei.sv", "4");
+        android::init::property_set("tether_dun_required", "0");
    }
 
     if (sku == "XT1621" || sku == "XT1622" || sku == "XT1640" || sku == "XT1642" || sku == "XT1643") {
         if (radio == "India") {
-            property_set("ro.radio.imei.sv", "9");
-            property_set("persist.radio.is_wps_enabled", "true");
+            android::init::property_set("ro.radio.imei.sv", "8");
+            android::init::property_set("persist.radio.is_wps_enabled", "true");
         } else {
-            property_set("ro.radio.imei.sv", "3");
+            android::init::property_set("ro.radio.imei.sv", "3");
         }
     }
 
     if (sku == "XT1626" || sku == "XT1641") {
-        property_set("ro.radio.imei.sv", "2");
-        property_set("persist.radio.is_wps_enabled", "true");
-        property_set("persist.radio.pb.max.match", "10");
+        android::init::property_set("ro.radio.imei.sv", "2");
+        android::init::property_set("persist.radio.is_wps_enabled", "true");
+        android::init::property_set("persist.radio.pb.max.match", "10");
     }
 }
 
@@ -123,43 +122,43 @@ static void target_ram(void) {
     ram = android::base::GetProperty("ro.boot.ram", "");
 
     if (ram == "2GB") {
-        property_set("dalvik.vm.heapstartsize", "8m");
-        property_set("dalvik.vm.heapgrowthlimit", "192m");
-        property_set("dalvik.vm.heapsize", "384m");
-        property_set("dalvik.vm.heaptargetutilization", "0.75");
-        property_set("dalvik.vm.heapminfree", "512k");
-        property_set("dalvik.vm.heapmaxfree", "8m");
+        android::init::property_set("dalvik.vm.heapstartsize", "8m");
+        android::init::property_set("dalvik.vm.heapgrowthlimit", "192m");
+        android::init::property_set("dalvik.vm.heapsize", "384m");
+        android::init::property_set("dalvik.vm.heaptargetutilization", "0.75");
+        android::init::property_set("dalvik.vm.heapminfree", "512k");
+        android::init::property_set("dalvik.vm.heapmaxfree", "8m");
 
-        property_set("ro.hwui.texture_cache_size", "72");
-        property_set("ro.hwui.layer_cache_size", "48");
-        property_set("ro.hwui.r_buffer_cache_size", "8");
-        property_set("ro.hwui.path_cache_size", "32");
-        property_set("ro.hwui.gradient_cache_size", "1");
-        property_set("ro.hwui.drop_shadow_cache_size", "6");
-        property_set("ro.hwui.texture_cache_flushrate", "0.4");
-        property_set("ro.hwui.text_small_cache_width", "1024");
-        property_set("ro.hwui.text_small_cache_height", "1024");
-        property_set("ro.hwui.text_large_cache_width", "2048");
-        property_set("ro.hwui.text_large_cache_height", "1024");
+        android::init::property_set("ro.hwui.texture_cache_size", "72");
+        android::init::property_set("ro.hwui.layer_cache_size", "48");
+        android::init::property_set("ro.hwui.r_buffer_cache_size", "8");
+        android::init::property_set("ro.hwui.path_cache_size", "32");
+        android::init::property_set("ro.hwui.gradient_cache_size", "1");
+        android::init::property_set("ro.hwui.drop_shadow_cache_size", "6");
+        android::init::property_set("ro.hwui.texture_cache_flushrate", "0.4");
+        android::init::property_set("ro.hwui.text_small_cache_width", "1024");
+        android::init::property_set("ro.hwui.text_small_cache_height", "1024");
+        android::init::property_set("ro.hwui.text_large_cache_width", "2048");
+        android::init::property_set("ro.hwui.text_large_cache_height", "1024");
     } else {
-        property_set("dalvik.vm.heapstartsize", "8m");
-        property_set("dalvik.vm.heapgrowthlimit", "192m");
-        property_set("dalvik.vm.heapsize", "384m");
-        property_set("dalvik.vm.heaptargetutilization", "0.75");
-        property_set("dalvik.vm.heapminfree", "512k");
-        property_set("dalvik.vm.heapmaxfree", "8m");
+        android::init::property_set("dalvik.vm.heapstartsize", "8m");
+        android::init::property_set("dalvik.vm.heapgrowthlimit", "192m");
+        android::init::property_set("dalvik.vm.heapsize", "384m");
+        android::init::property_set("dalvik.vm.heaptargetutilization", "0.75");
+        android::init::property_set("dalvik.vm.heapminfree", "512k");
+        android::init::property_set("dalvik.vm.heapmaxfree", "8m");
 
-        property_set("ro.hwui.texture_cache_size", "72");
-        property_set("ro.hwui.layer_cache_size", "48");
-        property_set("ro.hwui.r_buffer_cache_size", "8");
-        property_set("ro.hwui.path_cache_size", "32");
-        property_set("ro.hwui.gradient_cache_size", "1");
-        property_set("ro.hwui.drop_shadow_cache_size", "6");
-        property_set("ro.hwui.texture_cache_flushrate", "0.4");
-        property_set("ro.hwui.text_small_cache_width", "1024");
-        property_set("ro.hwui.text_small_cache_height", "1024");
-        property_set("ro.hwui.text_large_cache_width", "2048");
-        property_set("ro.hwui.text_large_cache_height", "2048");
+        android::init::property_set("ro.hwui.texture_cache_size", "72");
+        android::init::property_set("ro.hwui.layer_cache_size", "48");
+        android::init::property_set("ro.hwui.r_buffer_cache_size", "8");
+        android::init::property_set("ro.hwui.path_cache_size", "32");
+        android::init::property_set("ro.hwui.gradient_cache_size", "1");
+        android::init::property_set("ro.hwui.drop_shadow_cache_size", "6");
+        android::init::property_set("ro.hwui.texture_cache_flushrate", "0.4");
+        android::init::property_set("ro.hwui.text_small_cache_width", "1024");
+        android::init::property_set("ro.hwui.text_small_cache_height", "1024");
+        android::init::property_set("ro.hwui.text_large_cache_width", "2048");
+        android::init::property_set("ro.hwui.text_large_cache_height", "2048");
     }
 }
 
@@ -168,12 +167,12 @@ static void num_sims(void) {
 
     dualsim = android::base::GetProperty("ro.boot.dualsim", "");
 
-    property_set("ro.hw.dualsim", dualsim.c_str());
+    android::init::property_set("ro.hw.dualsim", dualsim.c_str());
 
     if (dualsim == "true") {
-        property_set("persist.radio.multisim.config", "dsds");
-        property_set("ro.telephony.ril.config", "simactivation");
+        android::init::property_set("persist.radio.multisim.config", "dsds");
+        android::init::property_set("ro.telephony.ril.config", "simactivation");
     } else {
-        property_set("persist.radio.multisim.config", "");
+        android::init::property_set("persist.radio.multisim.config", "");
     }
 }
