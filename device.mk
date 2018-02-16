2c9dbc1e86cb6b45a30e6ee5328aa780d6468ece
Nikit Sharma <nikitsharma92@gmail.com>
2018-02-17 04:15:12 +0530

    athene: Add custom camera provider to handle user parameter.

--- device.mk
+++ device.mk
@@ -111,7 +111,7 @@ PRODUCT_COPY_FILES +=  \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-impl-athene \
     camera.device@3.2-impl \
     libbson \
     libshims_camera \
